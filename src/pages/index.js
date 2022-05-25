983e7f0f842705043aaa9d2726644eb89a47e6f8
Andy Pearson <andy@londonparkour.com>
2022-05-25 08:17:12 +0100

    frontpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import Card from '../components/card'
+
 
 const IndexPage = () => {
 
   return (
     <Layout pageTitle="Home">
-      <p className='mb-10'>This is my first attempt at a Gatsby / Tailwind website.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />      
-
+      <h1 className="text-1xl text mb-4" >Latest Versions</h1>
+      <p className='mb-10'>This is my first attempt at a Gatsby / Tailwind website.</p>   
+      <Card/>
     </Layout>
   )
 }
